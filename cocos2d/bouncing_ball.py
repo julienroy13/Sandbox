c3374ea3e6450e58977f06a0fdbf4de140bb57db
julienroy13 <jul.roy1311@gmail.com>
2018-07-11 22:46:28 -0400

    Cursor click now verify if mouse is in circle (not sprite square). Balls blocks on the sides on the screen. Added args for window width and height.

--- cocos2d/bouncing_ball.py
+++ cocos2d/bouncing_ball.py
@@ -4,6 +4,9 @@ import os
 import argparse
 import pyglet
 
+DEFAULT_WIDTH = 1280
+DEFAULT_HEIGHT = 720
+
 class Background(cocos.layer.ColorLayer):
     # If you want that your layer receives director.window events, you must set this variable to 'True'
     is_event_handler = True
@@ -29,7 +32,7 @@ class BouncingBall(cocos.layer.Layer):
     # If you want that your layer receives director.window events, you must set this variable to 'True'
     is_event_handler = True
 
-    def __init__(self):
+    def __init__(self, args):
         super().__init__()
 
         # Creates a sprite
@@ -39,10 +42,17 @@ class BouncingBall(cocos.layer.Layer):
 
         # Adds the sprite to our layer (z is its position in an axis coming out of the screen)
         self.add(self.sprite, z=1)
+        
+        self.window_width = args.width
+        self.window_height = args.height
 
     def update_ball_position(self, dx, dy):
-        self.sprite.x += dx
-        self.sprite.y += dy
+        new_position_x = self.sprite.x + dx
+        new_position_y = self.sprite.y + dy
+        if new_position_x - self.sprite.width//2 > 0 and new_position_x + self.sprite.width//2 < self.window_width:
+            self.sprite.x += dx
+        if new_position_y - self.sprite.height//2 > 0 and new_position_y + self.sprite.height//2 < self.window_height:
+            self.sprite.y += dy
 
     # EVENT HANDLERS
     def on_key_press(self, key, modifiers):
@@ -68,10 +78,9 @@ class BouncingBall(cocos.layer.Layer):
         'buttons' is a bitwise-or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
         'modifiers' is a bitwise-or of pyglet.window.key modifier constants (values like 'SHIFT', 'OPTION', 'ALT')
         """
-        # TODO : Improve to make it so that the circle condition is a circle, not a square
-        mouse_in_circle_x = x > (self.sprite.x - self.sprite.width//2) and x < (self.sprite.x + self.sprite.width//2)
-        mouse_in_circle_y = y > (self.sprite.y - self.sprite.height//2) and y < (self.sprite.y + self.sprite.height//2)
-        if mouse_in_circle_x and mouse_in_circle_y:
+
+        # Checks if mouse is in the circle
+        if ((x - self.sprite.x)**2 + (y - self.sprite.y)**2 < (self.sprite.width//2)**2):
             self.update_ball_position(dx, dy)
 
 def args_check(args):
@@ -80,22 +89,27 @@ def args_check(args):
     :param args: args
     :return: args
     """
-    pass
+    if args.width < 50 or args.width > 2000:
+        raise ValueError(f'Parameter "args.width" should be between 50 and 2000. Got {args.width} instead')
+
+    if args.height < 50 or args.height > 1000:
+        raise ValueError(f'Parameter "args.height" should be between 50 and 1000. Got {args.height} instead')
 
     return args
 
 
 if __name__ == '__main__':
     # Parse args
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--tuto', default=DEFAULT_TUTO)
-    # args = args_check(parser.parse_args())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--width', default=DEFAULT_WIDTH)
+    parser.add_argument('--height', default=DEFAULT_HEIGHT)
+    args = args_check(parser.parse_args())
 
     # Initializes the director (whatever this does)
-    cocos.director.director.init(width=1280, height=720, caption='Bouncing Ball', resizable=True)
+    cocos.director.director.init(width=args.width, height=args.height, caption='Bouncing Ball', resizable=True)
 
     # Instantiates our layer and creates a scene that contains our layer as a child
-    main_scene = cocos.scene.Scene(Background(), BouncingBall())
+    main_scene = cocos.scene.Scene(Background(), BouncingBall(args))
 
     # We run our scene
     cocos.director.director.run(main_scene)
