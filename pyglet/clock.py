2d0a8044bc2a0ddc93e7f56c2a759ca751c4d5a5
JulienRoy <jul.roy1311@gmail.com>
2019-07-31 15:03:44 -0400

    Finished the little clock demo

--- pyglet/clock.py
+++ pyglet/clock.py
@@ -1,6 +1,5 @@
 from pyglet.gl import *
 from math import pi, sin, cos
-import numpy as np
 
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
@@ -8,13 +7,20 @@ WINDOW_HEIGHT = 500
 CARTESIAN_LIMIT = 1
 
 
-def cartesian_to_window(point):
-    x, y = point
-    return [(x + CARTESIAN_LIMIT) * WINDOW_WIDTH / (2 * CARTESIAN_LIMIT),
-            (y + CARTESIAN_LIMIT) * WINDOW_HEIGHT / (2 * CARTESIAN_LIMIT)]
+def get_clock_numbers(x, y, radius):
+    labels = []
+    for i in range(1, 13):
+        labels.append(pyglet.text.Label(str(i),
+                                        x=x + (radius * sin(i * 2. * pi / 12.)),
+                                        y=y + (radius * cos(i * 2. * pi / 12.)),
+                                        font_name='Times New Roman',
+                                        font_size=36,
+                                        anchor_x='center',
+                                        anchor_y='center'))
+    return labels
 
 
-def make_circle(x, y, radius, frame):
+def draw_full_circle(x, y, radius, frame):
     """
     We want a pixel perfect circle. To get one,
     we have to approximate it densely with triangles.
@@ -27,21 +33,24 @@ def make_circle(x, y, radius, frame):
     s = sin(2 * pi / iterations)
     c = cos(2 * pi / iterations)
 
-    dx, dy = radius * cos(frame / 100.), radius * sin(frame / 100.)
+    dx, dy = radius * sin(frame * pi / 180.), radius * cos(frame * pi / 180.)
 
     glBegin(GL_TRIANGLE_FAN)
     glVertex2f(x, y)
     for _ in range(iterations + 1 - 10):
         glVertex2f(x + dx, y + dy)
-        dx, dy = (dx * c - dy * s), (dy * c + dx * s)
+        dx, dy = (dx * c + dy * s), (dy * c - dx * s)
     glEnd()
 
 
-# get all the points in a circle centered at 0.
-def PointsInCircum(r, n=25):
-    return [(cos(2 * pi / n * x) * r, sin(2 * pi / n * x) * r) for x in range(0, n + 1)]
+def draw_full_rectangle(length, frame):
+    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [
+        (WINDOW_WIDTH / 2.) - (length / 5.) * sin((frame-100.) * pi / 1800.), (WINDOW_WIDTH / 2.) - (length / 5.) * cos((frame-100.) * pi / 1800.),  # point 1
+        (WINDOW_WIDTH / 2.) - (length / 5.) * sin((frame+100.) * pi / 1800.), (WINDOW_WIDTH / 2.) - (length / 5.) * cos((frame+100.) * pi / 1800.),  # point 2
+        (WINDOW_WIDTH / 2.) + length * sin((frame-5.) * pi / 1800.), (WINDOW_WIDTH / 2.) + length * cos((frame-5.) * pi / 1800.),  # point 3
+        (WINDOW_WIDTH / 2.) + length * sin((frame+5.) * pi / 1800.), (WINDOW_WIDTH / 2.) + length * cos((frame+5.) * pi / 1800.),  # point 4
+        ]))
 
-pts = np.array(PointsInCircum(WINDOW_WIDTH/2.))
 
 # function that increments the frame
 frame = 0
@@ -57,27 +66,30 @@ if __name__ == "__main__":
     # creates fps display
     fps_display = pyglet.clock.ClockDisplay()
 
+    # creates digit labels
+    labels = get_clock_numbers(WINDOW_WIDTH / 2., WINDOW_WIDTH / 2., WINDOW_WIDTH / 2.5)
+
     @window.event
     def on_draw():
         # clear the screen
         glClear(GL_COLOR_BUFFER_BIT)
 
+        # draws the background of the clock (a missing triangle is used for seconds)
         glColor3f(0, 0, 0.5)
-        make_circle(WINDOW_WIDTH / 2., WINDOW_HEIGHT / 2., WINDOW_WIDTH / 2., frame)
+        draw_full_circle(WINDOW_WIDTH / 2., WINDOW_HEIGHT / 2., WINDOW_WIDTH / 2., frame)
 
-        glColor3f(0, 0, 0.)
-        # draw the next line
-        # in the circle animation
-        # circle centered at 100,100,0 = x,y,z
-        glBegin(GL_LINES)
-        glVertex3f(250, 250, 0)
-        glVertex3f(pts[frame % pts.shape[0]][1] + 250, pts[frame % pts.shape[0]][0] + 250, 0)
-        glEnd()
+        # draws the numbers on the clock
+        for label in labels:
+            label.draw()
+
+        glColor3f(1., 1., 1.)
+
+        # draws the slower minute bar
+        draw_full_rectangle(150, frame)
 
         # draws fps display
         fps_display.draw()
 
-
     # every 1/10 th get the next frame
-    pyglet.clock.schedule(update_frame, 100.)
+    pyglet.clock.schedule(update_frame, WINDOW_WIDTH / 2.)
     pyglet.app.run()
